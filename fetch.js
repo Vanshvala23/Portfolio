3beba562e7bdc56a91e8868b7d5870d1863256e6
VanshVala <102245923+Vanshvala23@users.noreply.github.com>
2024-12-15 15:55:11 +0530

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const https = require("https");
 
-const GITHUB_TOKEN = "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";  // If you are using a token, provide it here, or leave it empty if not needed.
-const GITHUB_USERNAME = "Vanshvala23";  // Only the username, not the full URL
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN || "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";  // If you are using a token, provide it here, or leave it empty if not needed.
+const GITHUB_USERNAME = process.env.GITHUB_USERNAME||"Vanshvala23";  // Only the username, not the full URL
 const ERR = {
   noUserName:
     "Github Username was found to be undefined. Please set all relevant environment variables.",
